3af9a72dc606bf7b61dc0152a5d579ff1178ddfd
Alisher778 <alisher.musurmonov89@gmail.com>
2019-03-06 12:19:32 +0500

    changed the animation style to bounce

--- js/main.js
+++ js/main.js
@@ -135,32 +135,32 @@ Matter.Events.on(mConstraint, 'mousedown', function (event) {
       // For popup content
       if(a.label === 'me') {
         document.getElementById('me').style.display = 'block';
-        document.getElementById('me').className += ' animated slideInUp';
+        document.getElementById('me').className += ' animated bounce';
         mConstraint.body = null;
       }
       if(a.label === 'everyone-in') {
         document.getElementById('everyone-in').style.display = 'block';
-        document.getElementById('everyone-in').className += ' animated slideInUp';
+        document.getElementById('everyone-in').className += ' animated bounce';
         mConstraint.body = null;
       }
       if(a.label === 'safe-clean-water') {
         document.getElementById('safe-clean-water').style.display = 'block';
-        document.getElementById('safe-clean-water').className += ' animated slideInUp';
+        document.getElementById('safe-clean-water').className += ' animated bounce';
         mConstraint.body = null;
       }
       if(a.label === 'iam-2018') {
         document.getElementById('iam-2018').style.display = 'block';
-        document.getElementById('iam-2018').className += ' animated slideInUp';
+        document.getElementById('iam-2018').className += ' animated bounce';
         mConstraint.body = null;
       }
       if(a.label === 'music') {
         document.getElementById('music').style.display = 'block';
-        document.getElementById('music').className += ' animated slideInUp';
+        document.getElementById('music').className += ' animated bounce';
         mConstraint.body = null;
       }
       if(a.label === 'testimonials') {
         document.getElementById('testimonials').style.display = 'block';
-        document.getElementById('testimonials').className += ' animated slideInUp';
+        document.getElementById('testimonials').className += ' animated bounce';
         mConstraint.body = null;
       }
     }
