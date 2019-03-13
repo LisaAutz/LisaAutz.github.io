4c9c911af979f3aad250eb3cb670288a557042d8
Alisher778 <alisher.musurmonov89@gmail.com>
2019-03-13 18:43:44 +0500

    fixed safari laggibg issue

--- js/action.js
+++ js/action.js
@@ -28,15 +28,34 @@ firebase.initializeApp(config);
 // Initialize database
 var db = firebase.firestore();
 // Get all posts from posts array
-db.collection('posts').get().then(function(data) {
-    data.docs.forEach(function(item) {
-        document.getElementById('myposts').innerHTML += `
-        <div id=${item.id} class="pop-up">
-            <button data-target=${item.id} class="close-btn" onclick="closePopUp(this)">X</button>
-            <div class="pop-up-content">
-                ${item.data().content}
+
+if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) {
+    setTimeout(function() {
+        db.collection('posts').get().then(function(data) {
+            data.docs.forEach(function(item) {
+                document.getElementById('myposts').innerHTML += `
+                <div id=${item.id} class="pop-up">
+                    <button data-target=${item.id} class="close-btn" onclick="closePopUp(this)">X</button>
+                    <div class="pop-up-content">
+                        ${item.data().content}
+                    </div>
+                </div>
+                `
+            })
+        });
+    }, 4000)
+} else {
+    db.collection('posts').get().then(function(data) {
+        data.docs.forEach(function(item) {
+            document.getElementById('myposts').innerHTML += `
+            <div id=${item.id} class="pop-up">
+                <button data-target=${item.id} class="close-btn" onclick="closePopUp(this)">X</button>
+                <div class="pop-up-content">
+                    ${item.data().content}
+                </div>
             </div>
-        </div>
-        `
-    })
-});
+            `
+        })
+    });
+}
+
